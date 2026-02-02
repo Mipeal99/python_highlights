50e20bbaa3069854b8a8db60f7263f218a014fd8
Miguel <mipeal99@gmail.com>
2026-02-02 06:07:11 +0100

    Update adaptive_integration.py
    translate comments

--- adaptive_integration.py
+++ adaptive_integration.py
@@ -8,8 +8,8 @@ Created on Mon Nov 18 13:36:22 2019
 import numpy as np
 
 def simpson (a, b, m, f):
-    '''m numero de subintervalos'''
-    n = 2*m+1 #numero de nodos
+    '''Simpson's rule for numerical integration of the function f on the interval (a,b), m is the number of subintervals'''
+    n = 2*m+1 #number of nodes
     x = np.linspace(a, b, n)
     h = (b-a)/(m)
     inte = f(x[0]) + f(x[-1])
@@ -21,28 +21,29 @@ def simpson (a, b, m, f):
 
 a = 0
 b = 4
-f = lambda x: 0.5*(x**2)*np.exp(-x)
+f = lambda x: 0.5*(x**2)*np.exp(-x) #test function
 
 def adaptacion(f,a,b,tol):
-    '''método adaptativo para calcular integral con simpson'''
-    alpha=a #setup del intervalo inicial
+    '''adaptative step method to calculate an integral with Simpson's rule'''
+    alpha=a #initial interval setup
     beta=b
-    I=0
+    I=0 #the integral's value will be stored here and summed over the subintervals
     while alpha<b:
-         #estimacion de la integral
-        I1=simpson(alpha,beta,1,f) #con1 intervalo
-        I2=simpson(alpha,beta,2,f) #con2 intervalos
-        error=1/15.*abs(I1-I2)
-        if error<tol*(beta-alpha)/(b-a): #si la integral es buena
-            I+=I2 #nos añade el cacho bueno de la integral
-            alpha=beta #nuevo punto de inicio donde acabó el bueno
-            beta=b #el resto
-        else:
-            beta=(alpha+beta)/2. #cogemos un intervalo más pequeño
+         #estimation of the integral
+        I1=simpson(alpha,beta,1,f) #Simpson with 1 subinterval
+        I2=simpson(alpha,beta,2,f) #Simpson with 1 subintervals
+        error=1/15.*abs(I1-I2) #calculates the difference between both methods
+        if error<tol*(beta-alpha)/(b-a): #if the error is small enough i.e. the approximation is good
+            I+=I2 #adds the good chunk to the integral
+            alpha=beta #sets up a new interval
+            beta=b 
+        else: #if the approximation is bad, it picks a smaller interval
+            beta=(alpha+beta)/2. 
     return I
 
-x=adaptacion(f,a,b,1e-15)
+x=adaptacion(f,a,b,1e-15) #test run
 print('adaptacion', x)
 print('simpson', simpson(a,b,2000,f))
 print(1-x)
+
 print(13/np.e**4)
